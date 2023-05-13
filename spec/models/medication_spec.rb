54632350ad2c6b40f5508b8e8e2c78875e2e849b
Matt Enyeart <menyeart1@gmail.com>
2023-05-13 09:31:16 -0600

    test: create medication model spec

---
+++ spec/models/medication_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe Medication, type: :model do
+  describe 'validations' do
+    it { should validate_presence_of :name }
+    it { should validate_presence_of :date_prescribed }
+  end
+
+  describe 'relationships' do
+    it { belong_to :condition }
+  end
+end
