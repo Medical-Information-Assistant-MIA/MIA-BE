542d1b096b3b5826d562f3c684497695e68acd62
Matt Enyeart <menyeart1@gmail.com>
2023-05-13 08:48:09 -0600

    feat: Add initial table migrations

---
+++ db/migrate/20230511234819_create_medications.rb
@@ -0,0 +1,16 @@
+class CreateMedications < ActiveRecord::Migration[7.0]
+  def change
+    create_table :medications do |t|
+      t.references :condition, null: false, foreign_key: true
+      t.string :name
+      t.string :pseudonym
+      t.date :date_prescribed
+      t.string :dosage
+      t.string :frequency
+      t.boolean :as_needed
+      t.string :prescribed_by
+
+      t.timestamps
+    end
+  end
+end
