4569f67d8576c13229dc4703688ce9198242aa0d
KaraJH <karas.jones@yahoo.com>
2023-05-14 14:00:44 -0600

    test: Test factories

--- spec/factories/medications.rb
+++ spec/factories/medications.rb
@@ -1,12 +1,12 @@
-# FactoryBot.define do 
-#   factory :medication do 
-#     name {Faker::Lorem.word} #Faker::Science.element => Carbon ...it's sciencey
-#     pseudonym {Faker::Lorem.word}
-#     date_prescribed {}
-#     dosage {} #250mg {Faker::Number.within(range: 50..500) + "mg"}
-#     frequency {}
-#     as_needed {}
-#     # prescribed_by {} doctor's name? Maybe hard code the name in the test?
-#     condition 
-#   end
-# end
+FactoryBot.define do 
+  factory :medication do 
+    name {Faker::Lorem.word} #Faker::Science.element => Carbon ...it's sciencey
+    pseudonym {Faker::Lorem.word}
+    date_prescribed {Faker::Date.between(from: 2.days.ago, to: Date.today)}
+    dosage {"#{rand(50..500)} mg"} 
+    frequency {"Take #{rand(1..3)} per day."}
+    as_needed {Faker::Boolean.boolean}
+    # prescribed_by {} doctor's name? Maybe hard code the name in the test?
+    condition 
+  end
+end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,8 +1,12 @@
 FactoryBot.define do 
   factory :user do 
     name {Faker::Name.name}
-    email {Faker::Internet.email}
-    # email {"#{name}.example@email.com"}
+    email {"#{name.delete(" ").downcase}.example@email.com"}
   end
+end
 
+def user_with_three_conditions(conditions_count: 3) 
+  FactoryBot.create(:user) do |user|
+    FactoryBot.create_list(:condition, conditions_count, user: user)
+  end
 end
