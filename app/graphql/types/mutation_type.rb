60e0e16497abd42857174d46dc9fb63475857d9e
Matt Enyeart <menyeart1@gmail.com>
2023-05-16 15:01:02 -0600

    feat: add doctor to graphql mutation types

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -2,11 +2,11 @@
 
 module Types
   class MutationType < Types::BaseObject
-    # TODO: remove me
-    field :test_field, String, null: false,
-                               description: "An example field added by the generator"
-    def test_field
-      "Hello World"
+    field :create_doctor, mutation: Mutations::CreateDoctor
+
+    def create_doctor(input:)
+      doctor = Doctor.create(input.to_h)
+      return doctor
     end
   end
 end
