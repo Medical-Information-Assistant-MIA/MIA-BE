bd716398afb73c80c2f99ee4490e91b39b0ef4f7
Matt Enyeart <menyeart1@gmail.com>
2023-05-16 15:00:36 -0600

    feat: add logic to doctor type

--- app/graphql/types/doctors_type.rb
+++ app/graphql/types/doctors_type.rb
@@ -2,5 +2,11 @@
 
 module Types
   class DoctorsType < Types::BaseObject
+    field :id, Integer, null: false
+    field :conditionId, Integer, null: false
+    field :name, String, null: false
+    field :address, String, null: true
+    field :phone, String, null: true
+    field :category, String, null: true
   end
 end
