b5a64397706a3507349039a494da12505b43a896
Tori Enyart <torienyart@gmail.com>
2023-05-17 11:42:45 -0600

    Add mutation for creating a medication

--- app/graphql/mutations/create_medication.rb
+++ app/graphql/mutations/create_medication.rb
@@ -1,7 +1,8 @@
 module Mutations 
   class CreateMedication < BaseMutation 
-    argument :name, String, required: true
+
     argument :conditionId, Integer, required: true
+    argument :name, String, required: true
     argument :datePrescribed, String, required: false
     argument :dosage, String, required: false
     argument :frequency, String, required: false
@@ -10,8 +11,8 @@ module Mutations
     field :medication, Types::MedicationsType, null: true
     field :errors, [String], null: true 
 
-    def resolve(name:, conditionId:, date_prescribed:, dosage:, frequency:, prescribed_by:)
-      medication = Medication.new(name: name, condition_id: conditionId, datePrescribed: date_prescribed, dosage: dosage, frequency: frequency, prescribed_by: prescribedBy)
+    def resolve(conditionId:, name:, datePrescribed:, dosage:, frequency:, prescribedBy:)
+      medication = Medication.new(condition_id: conditionId, name: name, date_prescribed: datePrescribed, dosage: dosage, frequency: frequency, prescribed_by: prescribedBy)
 
       if medication.save
         {
--- app/graphql/types/medications_type.rb
+++ app/graphql/types/medications_type.rb
@@ -3,8 +3,9 @@
 module Types
   class MedicationsType < Types::BaseObject
     field :id, Integer, null: false
+    field :condition_id, Integer, null: false
     field :name, String, null: false
-    field :date_prescribed, String, null: true
+    field :date_prescribed, GraphQL::Types::ISO8601DateTime, null: true
     field :dosage, String, null: true
     field :frequency, String, null: true
     field :prescribed_by, String, null: true
