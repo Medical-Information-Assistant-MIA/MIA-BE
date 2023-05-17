65382e06672978fa92e6ed5b7870261497495e8b
Tori Enyart <torienyart@gmail.com>
2023-05-17 10:56:06 -0600

    Run updated medications table migration

--- db/schema.rb
+++ db/schema.rb
@@ -46,11 +46,9 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_16_003847) do
   create_table "medications", force: :cascade do |t|
     t.bigint "condition_id", null: false
     t.string "name"
-    t.string "pseudonym"
     t.date "date_prescribed"
     t.string "dosage"
     t.string "frequency"
-    t.boolean "as_needed"
     t.string "prescribed_by"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
