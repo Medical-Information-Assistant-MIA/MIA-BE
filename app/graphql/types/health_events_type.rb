e64235bc8ddc209d2ceef0dadb01500a6a0d3031
Matt Enyeart <menyeart1@gmail.com>
2023-05-17 11:05:37 -0600

    feat:add health event post

--- app/graphql/types/health_events_type.rb
+++ app/graphql/types/health_events_type.rb
@@ -2,5 +2,12 @@
 
 module Types
   class HealthEventsType < Types::BaseObject
+    field :id, Integer, null: false
+    field :note, String, null: false
+    field :date, String, null: false
+    field :condition_id, Integer, null: false
+    field :category, String, null: true
+    field :created_at, GraphQL::Types::ISO8601DateTime, null: false
+    field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
   end
 end
