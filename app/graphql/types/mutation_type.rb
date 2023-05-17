75d052fc6cd2fbe761a79e5014e1bd20f13e3d8d
KaraJH <karas.jones@yahoo.com>
2023-05-17 09:52:55 -0600

    feat: Add create doctor mutation

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -3,6 +3,7 @@
 module Types
   class MutationType < Types::BaseObject
     field :create_user, mutation: Mutations::CreateUser
+    field :create_doctor, mutation: Mutations::CreateDoctor
     field :test_field, String, null: false, description: "An example field added by the generator"
 
     def test_field
