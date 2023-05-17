bd17640a2fe0c1cccffe75322dec5604f9d57134
Brad Dunlap <braddunlapdpt@gmail.com>
2023-05-17 12:40:24 -0400

    add relationships to users

---
+++ app/graphql/mutations/create_condition.rb
@@ -0,0 +1,25 @@
+module Mutations
+	class CreateCondition < BaseMutation
+		argument :name, String, required: true
+		argument :userId, Integer, required: true
+
+		field :condition, Types::ConditionsType, null: true
+		field :errors, [String], null: true
+
+		def resolve(name:, conditionId:)
+			condition = Condition.new(name: name, user_id: userId)
+
+			if condition.save
+				{
+					condition: condition
+					errors: []
+				}
+			else 
+				{
+					condition: nil
+					errors: condition.errors.full_messages
+				}
+			end
+		end
+	end
+end
