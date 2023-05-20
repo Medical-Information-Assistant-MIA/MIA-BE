6387bc5f48d2713333783257980c0a3d0e1a0b75
Matt Enyeart <menyeart1@gmail.com>
2023-05-20 09:58:19 -0600

    feat: create condition destroy mutation

---
+++ app/graphql/mutations/destroy_condition.rb
@@ -0,0 +1,22 @@
+module Mutations
+  class DestroyCondition < BaseMutation
+    argument :id, Integer, required: true
+
+    field :success, Boolean, null: false
+    field :errors, [String], null: true
+
+    def resolve(id:)
+      condition = Condition.find_by(id: id)
+
+      if condition
+        if condition.destroy
+          { success: true, errors: [] }
+        else
+          { success: false, errors: condition.errors.full_messages }
+        end
+      else
+        { success: false, errors: ["Condition not found"] }
+      end
+    end
+  end
+end
