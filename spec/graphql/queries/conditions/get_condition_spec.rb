7ddd5eb23a117acfc8666cd90d13471c70c3414b
Brad Dunlap <braddunlapdpt@gmail.com>
2023-05-20 11:05:46 -0400

    feat: testing for user query

--- spec/graphql/queries/conditions/get_condition_spec.rb
+++ spec/graphql/queries/conditions/get_condition_spec.rb
@@ -1,4 +1,5 @@
 require 'rails_helper'
+require 'action_dispatch'
 
   module Queries
     module Conditions
---
+++ spec/graphql/queries/conditions/get_user_spec.rb
@@ -0,0 +1,39 @@
+require 'rails_helper'
+require 'action_dispatch'
+
+module Queries
+  module Users
+    RSpec.describe User, type: :request do
+      include ActionDispatch::Integration::RequestHelpers
+      
+      describe '.resolve' do
+        it 'gets a user' do
+          user = create(:user)
+          keys = [:id, :name, :email, :conditions]
+
+          post '/graphql', params: { query: query(user.id) }
+
+          data = JSON.parse(response.body, symbolize_names: true)
+
+          expect(response).to be_successful
+          expect(data[:data][:user].keys).to eq(keys)
+        end
+      end
+
+      def query(id)
+        <<~GQL
+          {
+            user(id: #{id}) {
+              id
+              name
+							email
+              conditions {
+                name
+              }
+            }
+          }
+        GQL
+      end
+    end
+  end
+end
